ad858c1dc7acb41cdac66ad2ec8529cdeacabf3a
vilyever <250450035@163.com>
2016-08-05 09:24:55 +0800

    Initial Commit

---
+++ objcView/objcView.h
@@ -0,0 +1,19 @@
+//
+//  objcView.h
+//  objcView
+//
+//  Created by FTET on 16/8/5.
+//  Copyright © 2016年 vilyever. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for objcView.
+FOUNDATION_EXPORT double objcViewVersionNumber;
+
+//! Project version string for objcView.
+FOUNDATION_EXPORT const unsigned char objcViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <objcView/PublicHeader.h>
+
+
